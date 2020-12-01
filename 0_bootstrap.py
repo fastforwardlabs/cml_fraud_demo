8b6f5dc545024d7ca7ac7ad5355a0393f40c722a
andrewrreed <areed1242@gmail.com>
2020-12-01 10:35:12 -0500

    correct bootstrap for cdsw users

--- 0_bootstrap.py
+++ 0_bootstrap.py
@@ -32,12 +32,14 @@ cml = CMLBootstrap(HOST, USERNAME, API_KEY, PROJECT_NAME)
 try : 
   storage=os.environ["STORAGE"]
 except:
-  tree = ET.parse('/etc/hadoop/conf/hive-site.xml')
-  root = tree.getroot()
-    
-  for prop in root.findall('property'):
-    if prop.find('name').text == "hive.metastore.warehouse.dir":
+  if os.path.exists("/etc/hadoop/conf/hive-site.xml"):
+    tree = ET.parse('/etc/hadoop/conf/hive-site.xml')
+    root = tree.getroot()
+    for prop in root.findall('property'):
+      if prop.find('name').text == "hive.metastore.warehouse.dir":
         storage = prop.find('value').text.split("/")[0] + "//" + prop.find('value').text.split("/")[2]
+  else:
+    storage = "/user/" + os.getenv("HADOOP_USER_NAME")
   storage_environment_params = {"STORAGE":storage}
   storage_environment = cml.create_environment_variable(storage_environment_params)
   os.environ["STORAGE"] = storage
